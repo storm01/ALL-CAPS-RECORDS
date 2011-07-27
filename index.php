54c86ffc2ecac10a02c4b1d6bd2bfb24a8c545b1
Will Dayble <will@willdayble.com>
2011-07-27 18:05:25 +1000

    now has a home paeg, and move stuff around also plz

---
+++ index.php
@@ -0,0 +1,51 @@
+<?php include_once('templates/header.php'); ?>
+
+	<div class="m1">
+		<a href="http://allcaps.fm" class="allcaps">ALL_CAPS_RECORDS:_</a>
+		<a class="active" href="releases">releases</a>
+		<a class="" href="artists">artists</a>
+		<a class="" href="about">about</a>
+		<a class="" target="_blank" href="http://allcapsrecords.bandcamp.com">buy shit</a>
+	</div>
+
+	<div class="penises">
+		<h1>ALL CAPS RECORDS BITCHES</h1>
+		<div class="durr">
+			<p>Bass music, Chiptunes, electro, video game tunes and various *step coming soon.</p>
+			<p class="fuckoff"><a href="http://allcapsrecords.bandcamp.com/" target="_blank">Buy stuff at bandcamp</a></p>
+		</div>	
+	</div>
+
+<!--fuckyou--></div>
+
+
+<div class="releases">
+
+	<div class="release">
+	
+		<a class="cover" href="http://allcapsrecords.bandcamp.com/track/four-litres-of-hope"><img src="design/images/releases/001.jpg" alt="001"></a>
+
+		<h2><strong>ALLCAPS001:</strong> Four Litres of Hope </h2>
+		
+		<div class="stuff">
+		
+			<div class="soundcloud">
+				<object height="81" width="100%"> <param name="movie" value="http://player.soundcloud.com/player.swf?url=http%3A%2F%2Fapi.soundcloud.com%2Ftracks%2F18636338"></param> <param name="allowscriptaccess" value="always"></param> <embed allowscriptaccess="always" height="81" src="http://player.soundcloud.com/player.swf?url=http%3A%2F%2Fapi.soundcloud.com%2Ftracks%2F18636338" type="application/x-shockwave-flash" width="100%"></embed> </object> 
+			</div>
+			<p><a href="http://soundcloud.com/bankaibash/four-litres-of-hope">Bankai vs. Abortifacient - Four Litres of Hope</a></p>
+			<p><a href="http://allcapsrecords.bandcamp.com/track/four-litres-of-hope" target="_blank">Buy it at bandcamp</a></p>
+			
+			<div class="floh">
+				<a target="_blank" href="http://fourlitresofhope.com"><img src="http://fourlitresofhope.com/awesome/freshprince.gif"></a>
+				Watch the animated gif experience at <a target="_blank" href="http://fourlitresofhope.com">fourlitresofhope.com</a>
+			</div>
+		</div>
+
+	</div>
+
+</div>
+
+
+
+
+<?php include_once('templates/footer.php'); ?>
