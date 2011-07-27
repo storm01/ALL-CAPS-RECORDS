35f6d70a8b1092d519e21264164d38864f0f6d4a
Will Dayble <will@willdayble.com>
2011-07-27 18:18:44 +1000

    move scripts to header

--- templates/header.php
+++ templates/header.php
@@ -13,6 +13,23 @@
 
 	<link rel="shortcut icon" href="design/images/favicon.ico"/>
 	
+	<script type="text/javascript" src="http://use.typekit.com/idy0qzx.js"></script>
+	<script type="text/javascript">try{Typekit.load();}catch(e){}</script>	
+	
+	<script type="text/javascript">
+	
+	  var _gaq = _gaq || [];
+	  _gaq.push(['_setAccount', 'UA-5821577-27']);
+	  _gaq.push(['_trackPageview']);
+	
+	  (function() {
+	    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+	    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+	    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+	  })();
+	
+	</script>	
+	
 </head>
 
 <body>
