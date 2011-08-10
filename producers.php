cc0b2230cfc98aa7e90940e303b700eceebba2e6
Will Dayble <will@willdayble.com>
2011-08-10 18:49:12 +1000

    added info for producers

---
+++ producers.php
@@ -0,0 +1,174 @@
+<?php include_once('templates/header.php'); ?>
+
+	<div class="m1">
+		<a href="http://allcaps.fm" class="allcaps">ALL_CAPS_RECORDS:_</a>
+		<a class="" href="releases">releases</a>
+		<a class="" href="artists">artists</a>
+		<a class="" href="about">about</a>
+		<a class="" target="_blank" href="http://allcapsrecords.bandcamp.com">buy shit</a>
+	</div>
+
+	<div class="penises">
+		<h1>ALL CAPS RECORDS BITCHES</h1>
+		<div class="durr">
+			<p>Information for producers and remixers.</p>
+			<p class="fuckoff">If you're releasing stuff with us, you need to know this</p>
+		</div>	
+	</div>
+
+<!--fuckyou--></div>
+
+
+
+<div class="bitches bitches_with_menu">
+
+	<div class="menu">
+		<a href="#happs">What happens when you release with us</a>
+		<a href="#cc">Creative Commons</a>
+		<a href="#upfront">Money - paid up front</a>
+		<a href="#shares">Money - sharing profits</a>	
+	</div>
+
+	<div class="boring">
+		<p>Here's a bunch of stuff you need to know if you're planning to release tracks via ALL CAPS. We do not 'sign' artists and contracts are on a release-by-release basis.</p>
+	</div>
+
+	<h2 id="happs">When you release with us:</h2>
+
+	<div class="boring">
+		<h4>You provide us with:</h4>
+		
+		<ol>
+			<li>Your final master .wav file(s)</li>
+			<li>Artwork, if you're supplying it.</li>
+			<li>Lyrics or notes  in plain text format</li>
+			<li>A signed copy of our release agreement</li>
+			<li>Any extras we've agreed to release such as synths, stems, digital artwork etc.</li>
+			<li>A guarantee that all the content you're provided is yours and you own the right to release it. For instance, if you use an audio sample from someone else's track, you have approval for it.</li>
+		</ol>
+		
+		<p>The above applies to both remixes and originals.</p>
+		
+	</div>		
+	
+	<h2>Release agreement:</h2>
+
+	<div class="boring">
+	
+		<p>The 'release agreement' is a contract. It outlines this stuff, in legal talk:</p>
+	
+		<h4>Stuff we can do:</h4>
+		
+		<ul>
+			<li>We have exclusive right to distribute the track, digitally and physically.</li>
+			<li>We have exclusive right to sell the release via whatever means we want. Examples include: compilation albums, combined digital downloads, physical merchandise, mixes.</li>
+			<li>Use your name/logo/picture as an 'ALL CAPS Artist' on our website, Facebook, Soundcloud printed material, etc.</li>
+		</ul>
+		
+		<h4>Stuff we can't do:</h4>
+		
+		<ul>
+			<li>Sell or try to profit from any of your material that isn't released via ALL CAPS.</li>
+		</ul>
+		
+		<hr>
+		
+		<h4>Stuff you can do:</h4>
+		<ul>
+			<li>You can preview the track anywhere you want online (youtube, soundcloud, etc) without allowing downloads, after it's approved for release by ALL CAPS.</li>		
+		</ul>		
+
+
+		<h4>Stuff you can't do:</h4>
+		
+		<ul>
+			<li>Release or sell anything to do with the release via another label, Bandcamp, Beatport, or similar. All money goes through us!</li>
+			<li>Give the track away for free anywhere, unless we say it's okay.</li>
+		</ul>
+		
+	</div>		
+	
+	<h2 id="cc">Creative Commons</h2>
+	
+	<div class="boring">
+	
+		<p>We may, at any time, release or re-release anything our artists provide us with (tracks, samples, synths, art) under a Creative Commons License.</p>
+		<p>For instance, we may release a single for purchase-only via Beatport, then a month later change our mind and give it away for free under a Creative Commons license.</p>
+		<p>Usually, we'll use a <a href="http://creativecommons.org/licenses/by-nc-sa/2.0/" target="_blank">BY-NC-SA</a> license.</p>
+		
+	</div>
+	
+
+	<h2 id="upfront">Money - getting paid up front:</h2>
+	
+	<div class="boring">
+				
+		<h4>If you are being paid up front (common for remixes and singles):</h4>
+		
+		<ul>
+			<li>We agree on a fee for the release.</li>
+			<li>We pay 50% up front and 50% on receiving an acceptable master.</li>
+			<li>From that point on we can do pretty much anything we want with the release.</li>
+		</ul>
+	
+	</div>
+	
+	<h2 id="shares">Money - Sharing profits:</h2>
+	
+	<div class="boring">
+	
+		<h4>If you are taking a share of profits (common for full EPs/LPs):</h4>
+		
+		<p>This is slightly more complex. Put simply, we cover all the associated costs to do with the release.</p>
+		
+		<p>Some examples of costs include:</p>
+
+		<ul>
+			<li>Paying other artists for remixes</li>
+			<li>Mastering costs</li>
+			<li>Printing costs, if it's physical</li>
+			<li>Paying off mafia bosses, corrupt governments, etc (this is a joke).</li>
+		</ul>
+		
+		<p><strong>After</strong> all release-related expenses are paid for, everyone gets their cut of profit.</p>
+		
+		<hr>
+		
+		<h4>If it's your release:</h4>
+		<p>We take 30%, you take 70%. This is for the life of the release.</p>
+		<p><img src="design/images/producers/30-70-split.jpg" alt="30-70-split"></p>
+
+		<hr>
+		
+		<h4>If it's a collaboration with another artist:</h4>
+		<p>We take 30%, then you and the other artist take half each, or 35%.</p>
+		<p><img src="design/images/producers/30-35-35-split.jpg" alt="30-35-35-split"></p>
+
+		<hr>
+		
+		<h4>If it's a remix of one of your tracks and we're NOT paying the remixer up front:</h4>
+		<p>We take 30%, you take 35% and the remixer takes 35%</p>
+		<p><img src="design/images/producers/30-35-remix.jpg" alt="30-35-remix"></p>	
+	
+	</div>
+	
+	
+	<h2 id="talk">Get in touch</h2>
+	
+	<div class="boring">
+
+
+		<p>Contact me directly with questions:</p>
+		<ul>
+			<li>Email: <a href="mailto:will@allcaps.fm">will@allcaps.fm</a></li>
+			<li>AIM: <strong>will@bankai.fm</strong></li>
+			<li>MSN: <strong>msn@bankai.fm</strong></li>
+		</ul>
+		
+	</div>
+
+</div><!--bitches-->
+
+
+
+<?php include_once('templates/footer.php'); ?>
